f2f0c5d943d2c69fe7012f7cff342f33158db6dc
imekenye <ismekenye@gmail.com>
2019-04-21 13:29:06 -0700

    add routing to view file

---
+++ app/main/views.py
@@ -0,0 +1,12 @@
+from flask import  render_template
+from . import main
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to Pitch'
+
+    return render_template('index.html', title=title)
