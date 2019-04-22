0abb029326d528c509e406416d89ae268d9e4207
imekenye <ismekenye@gmail.com>
2019-04-21 18:58:43 -0700

    update view file

--- app/main/views.py
+++ app/main/views.py
@@ -11,3 +11,12 @@ def index():
     title = 'Home - Welcome to Pitch'
 
     return render_template('index.html', title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
