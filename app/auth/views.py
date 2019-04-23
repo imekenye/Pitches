d54dd61478b062a1b5af7aec2ea3990cb488968a
imekenye <ismekenye@gmail.com>
2019-04-23 08:31:30 -0700

    import mail_message

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,8 @@ from ..models import User
 from .forms import LoginForm,RegistrationForm
 from .. import db
 from . import auth
+from ..email import mail_message
+
 
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -13,6 +15,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to Pitch","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
@@ -28,7 +33,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "pitch login"
+    title = "Pitch Login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 
