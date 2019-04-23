ad5644cdb648cf820125c7d6893058b2354686da
imekenye <ismekenye@gmail.com>
2019-04-23 08:24:18 -0700

    update config file

--- config.py
+++ config.py
@@ -3,7 +3,14 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:nathangwaro17@localhost/pitch'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    CSRF_ENABLED = True
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     pass
