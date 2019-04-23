e252565fc8c08f3b716e91282ba190f4703afcbd
imekenye <ismekenye@gmail.com>
2019-04-23 08:23:59 -0700

    add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+
+mail = Mail()
+bootstrap = Bootstrap()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -9,15 +15,24 @@ login_manager.login_view = 'auth.login'
 
 db = SQLAlchemy()
 
+photos = UploadSet('photos',IMAGES)
+
 def create_app(config_name):
     app = Flask(__name__)
 
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
     # initializing flask extensions
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
 
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+    # configure UploadSet
+    configure_uploads(app, photos)
+
+
+
 
     # Registering blueprints
     from .main import main as main_blueprint
