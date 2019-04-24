3865863e514e8448e6db0ee12e7d6e9ec02afd6d
imekenye <ismekenye@gmail.com>
2019-04-23 18:56:18 -0700

    deplyoment to heroku

--- config.py
+++ config.py
@@ -13,11 +13,12 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:nathangwaro17@localhost/pitch'
 
 config_options = {
 'development':DevConfig,
